652955375ea8bb279f568466d16c4d8706e96655
astra-b <mbavier@mail.pima.edu>
2024-09-25 12:38:31 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
-print('Hello, World!')
+#Author: Astra Bavier
+#Short story for CIS129
+print('Once upon a time there was a computer')
+print('He dreamed of traveling the world and meeting all kinds of people')
+print('But he couldn't since he was a computer')
+print('The end')
